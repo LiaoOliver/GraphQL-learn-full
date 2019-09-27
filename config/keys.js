75352f0345f8906c0a62eb9fa50cf0fd40478881
OliverLiao <rtw0913@gmail.com>
2019-09-27 23:53:25 +0800

    connect mongo DB

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+    mongoURL: "mongodb://restful:rest2018@ds143293.mlab.com:43293/restful-practice",
+    privateKey: "secret",
+}
--- index.js
+++ index.js
@@ -1,18 +1,32 @@
-const expressPlayground = require('graphql-playground-middleware-express').default;
 const { ApolloServer } = require('apollo-server-express');
+const expressPlayground = require('graphql-playground-middleware-express').default;
+const express = require('express');
+const { readFileSync } = require('fs');
+const MongoClient = require('mongodb').MongoClient;
 
+const typeDefs = readFileSync('./typeDefs.graphql', 'UTF-8');
+const resolvers = require('./resolver');
+const dbURL = require('./config/keys').mongoURL;
 
 const app = express();
-const server = new ApolloServer({
-    typeDefs,
-    resolvers
-})
-server.applyMiddleware({app});
 
-app.get('/', (req, res) => {
-    res.send("Welcome, to Photo share API");
-})
-
-app.get('/playground', expressPlayground({ endpoint:'/graphql'}));
-
-app.listen({ port: 4000 }, () => console.log(`GraphQL Service running on ${port}`))
+async function start(){
+    const client = await MongoClient.connect(dbURL, { useNewUrlParser: true, useUnifiedTopology: true })
+    const db = client.db();
+    const context = { db };
+    const server = new ApolloServer({
+        typeDefs,
+        resolvers,
+        context
+    })
+    server.applyMiddleware({app});
+    
+    app.get('/', (req, res) => {
+        res.send("Welcome, to Photo share API");
+    })
+    
+    app.get('/playground', expressPlayground({ endpoint:'/graphql' }));
+    
+    app.listen({ port: 4000 }, () => console.log(`GraphQL Service running on 4000`))
+}
+start();
